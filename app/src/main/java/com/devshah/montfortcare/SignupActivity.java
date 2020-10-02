bb1a1f5de488a67f6dfe5020f0c6dbd392a45150
devshah2 <35283231+devshah2@users.noreply.github.com>
2020-10-02 19:46:34 +0530

    server code added

--- app/src/main/java/com/devshah/montfortcare/SignupActivity.java
+++ app/src/main/java/com/devshah/montfortcare/SignupActivity.java
@@ -7,7 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+
 public class  SignupActivity extends AppCompatActivity {
+
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +45,41 @@ public class  SignupActivity extends AppCompatActivity {
                 x=findViewById(R.id.pwd2);
                 String pwd2=x.getText().toString();
 
-                if(cName!="" && pName!="" && contact!="" && address!="" && pwd!="" && pwd2==pwd){
+                if(cName!="" && pName!="" && contact!="" && address!="" && pwd!="" && pwd2.equals(pwd)){
+                    String url="http://192.168.1.15:8080/signup?cName="+cName+"&pName="+pName+"&contact="+contact+"&address="+address+"&pwd="+pwd;
                     //run query
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
+
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+                            if(!response.isSuccessful()){
+                                throw new IOException("Unexpected code " + response);
+                            }
+                            else{
+                                System.out.println(response.body().string());
+//                                response.body().
+                            }
+                        }
+                    });
+
+                }
+                else{
+                    System.out.println(pwd);
+                    System.out.println(pwd2);
+                    System.out.println(cName);
+                    System.out.println(pName);
+                    System.out.println(contact);
+                    System.out.println(address);
+
+                    System.out.println("Error");
                 }
             }
         });
