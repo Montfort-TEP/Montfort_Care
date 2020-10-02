3d70e97593b8ed79c63943df183dfb4d93c71d1f
devshah2 <35283231+devshah2@users.noreply.github.com>
2020-10-02 19:46:22 +0530

    Merge branch 'running' of /Users/dev/AndroidStudioProjects/Montfort_Care_TEP with conflicts.

--- app/src/main/java/com/devshah/montfortcare/MainActivity.java
+++ app/src/main/java/com/devshah/montfortcare/MainActivity.java
@@ -2,13 +2,24 @@ package com.devshah.montfortcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/devshah/montfortcare/SignupActivity.java
+++ app/src/main/java/com/devshah/montfortcare/SignupActivity.java
@@ -3,6 +3,9 @@ package com.devshah.montfortcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class  SignupActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class  SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Button next =  findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText x=findViewById(R.id.cNamee);
+                String cName=x.getText().toString();
+
+                x=findViewById(R.id.pName);
+                String pName=x.getText().toString();
+
+                x=findViewById(R.id.contact);
+                String contact=x.getText().toString();
+
+                x=findViewById(R.id.address);
+                String address=x.getText().toString();
+
+                x=findViewById(R.id.pwd);
+                String pwd=x.getText().toString();
+
+                x=findViewById(R.id.pwd2);
+                String pwd2=x.getText().toString();
+
+                if(cName!="" && pName!="" && contact!="" && address!="" && pwd!="" && pwd2==pwd){
+                    //run query
+                }
+            }
+        });
     }
 }
