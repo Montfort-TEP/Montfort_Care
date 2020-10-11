e75afbbea514c56b8039b91268dce34fa397e64f
kanishkkundu <Kanishkkundu123@gmail.com>
2020-10-12 01:00:33 +0530

    Main Activity Dashboard + Drawer menu Still need to do basic shit like extract hard coded string resources, create ID's ect.(will do it tommorrow during school)

--- app/src/main/java/com/devshah/montfortcare/MainActivity.java
+++ app/src/main/java/com/devshah/montfortcare/MainActivity.java
@@ -1,25 +1,46 @@
 package com.devshah.montfortcare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerlayout;
+    NavigationView navigationView;
+    Toolbar tool;
     Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signup = (Button) findViewById(R.id.signup);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(intent);
-            }
-        });
+        drawerlayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        tool = findViewById(R.id.toolbar);
+        navigationView.bringToFront();
+        setSupportActionBar(tool);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerlayout, tool, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        navigationView.bringToFront();
+        drawerlayout.addDrawerListener(toggle);
+        navigationView.bringToFront();
+        toggle.syncState();
+        navigationView.bringToFront();
     }
 }
+
+
+
+//        signup = (Button) findViewById(R.id.signup);
+//        signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent= new Intent(MainActivity.this, SignupActivity.class);
+//                startActivity(intent);
+//            }
+//        });
